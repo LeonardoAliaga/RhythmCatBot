c0613e676b1dfa9a0957ecbc8b2eefae6f9bb157
Aliaga2127 <jhersonaliaga18@gmail.com>
2024-03-20 18:22:31 -0500

    Actualizar WhatsappClient.js
    Agregar comandos

--- Whatsapp/WhatsappClient.js
+++ Whatsapp/WhatsappClient.js
@@ -26,10 +26,73 @@ whatsappClient.on("auth_failure", (error) => {
   console.error(error);
 });
 
-whatsappClient.on("message", (message) => {
-  // Maneja los mensajes de WhatsApp
-  // ...
-});
+whatsappClient.on('message_create', async (msg) => {
+    const { from, to, body, hasMedia } = msg;
+    let send;
+   (from === '51928750742@c.us') ? send = to : send = from;
+    const args = body.split(' ');
+    const content = msg.body.replace(args[0], '');
+
+    switch (args[0]) {
+      case prefix + 'wiki':
+        if(args[1]){
+          let doc = await Wiki.fetch(args[1], 'es')
+          sendMessage(send, doc.text());
+        } else if(!args[1]){
+          sendMessage(send, 'Debes de escribir lo que quieras bucar. *Ejemplo:*\n_y/wiki fotosintesis_');
+        }
+      break;
+      case prefix + 'img':
+        //console.log(await msg.getQuotedMessage())
+        var quoted = await msg.getQuotedMessage()
+        if(!msg.hasQuotedMsg) return sendMessage(send, 'Debes de reponder a una imagen');
+        if(!quoted.hasMedia) return sendMessage(send, 'Debes de reponder a una imagen')
+        if(!args[1]) return sendMessage(send, 'Debes de escribir el nombre que quieres darle al sticker');
+        if (quoted.hasMedia) {
+          const mediafile = await quoted.downloadMedia();
+          // console.log(
+          //   mediafile.mimetype,
+          //   mediafile.data.length
+          // );
+          //How to save that object as a file? =====================================
+    
+          fs.writeFile("./public/upload/" + msg.timestamp + ".png", mediafile.data, "base64", (err) => {
+              if (err) {
+                console.log(err);
+              }
+          convertToSticker(send, "./public/upload/" + msg.timestamp + ".png", content);
+            }
+          );
+          sendMessage(send, `Tu sticker **${content}** ya esta listo!`);
+          //========================================================================
+        } else {
+          sendMessage(send, 'Debes de reponder a una imagen');
+        }
+        
+        break;
+    }
+  })
+
+
+const convertToSticker = (to, file, name) => {
+  console.log(file)
+  const mediaFile = MessageMedia.fromFilePath(`${file}`)
+  client.sendMessage(to, mediaFile, {
+    sendMediaAsSticker: true,
+    stickerAuthor: 'PanquiBot',
+    stickerName: name,
+  })
+
+  
+  var path = file;
+  fs.unlink(path, function (err) {
+    if (err) {
+       console.error(err);
+     } else {
+     }
+   });
+}
+
 
 const sendMessage = (to, message) => {
   whatsappClient.sendMessage(to, `${message}\n\n_RhythmCatBot_`);
